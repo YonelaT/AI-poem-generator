c23d1f2bfadba532aea39123a6c6c3b991b81004
Yonela Tsutsu <136339535+YonelaT@users.noreply.github.com>
2024-11-04 18:50:12 +0200

    Added AI API integration

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function generatePoem(event)
-{
-    event.preventDefaault();
+ 
+function displayPoem(response){
     new Typewriter('#h1', {
-        strings: "let me not to the marriage of true love",
+        strings: response.data.answer,
         autoStart: true,
         cursor:"",
         delay:30,
       });
+}
+function generatePoem(event)
+{
+    event.preventDefaault();
+    let instructionInput=document.querySelector("#user-instructions")
+ 
+    let apiKey="ae3df4fe6d37bc70o28te4b4ca16cf4e"
+    let prompt=`Generate an English about love ${instructionInput.value}`;
+    let context=`Generatean english poem then add <br> between sentences and then  sign with <strong>SheCodes AI</strong> at the bottom`;
+    let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`
+    
+    axios.get(apiURL).then(displayPoem);
+
+   
     }
 let poemElement=document.querySelector("#poem-generator-form");
 poemElement.addEventListener("submit",generatePoem);
